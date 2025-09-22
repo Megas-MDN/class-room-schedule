90460de37c40744b6fdb9be6796b25c31506a173
Melquisedeque (Megas) do Nascimento <71480330+Megas-MDN@users.noreply.github.com>
2025-09-21 23:41:46 -0300

    Update app.js
    PORT 3001

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const db = require("./database/connection");
 const RoomService = require("./services/RoomService");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Inicializar banco
 async function init() {
